5fd2cf495ffbfee38ff1ee7a0cc9d81663a4bad9
Nabeelah <noblenab13@gmail.com>
2020-01-02 21:07:33 +0100

    Set up action and reducer files

---
+++ src/actions/events.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { apiURL } from '../config';
+
+export const FETCH_LOCATIONS_LOADING = 'FETCH_LOCATIONS_LOADING';
+export const FETCH_LOCATIONS_SUCCESS = 'FETCH_LOCATIONS_SUCCESS';
+export const FETCH_LOCATIONS_FAILURE = 'FETCH_LOCATIONS_FAILURE';
---
+++ src/components/EventCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const EventCard = props => {
+  return <div></div>;
+};
+
+
+const mapStateToProps = ({ Reducer }) => ({
+  
+});
+
+export default connect(null, mapStateToProps)(EventCard);
