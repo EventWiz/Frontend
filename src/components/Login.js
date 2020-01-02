c078afb52bff7e79027ef90d86166470a1ee0bc7
Noble Obioma <nobioma1@gmail.com>
2020-01-02 19:24:09 +0100

    Create Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import { useFormik } from 'formik';
+import React from 'react';
+import * as Yup from 'yup';
+import { Button } from '../styles/shared/Button';
+import { Form } from '../styles/shared/Form';
+import { Input } from '../styles/shared/Input';
+
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Email Address Required'),
+      password: Yup.string().required('Invalid Password'),
+    }),
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <h1>Log in</h1>
+      {formik.touched.email && formik.errors.email ? (
+        <p>{formik.errors.email}</p>
+      ) : null}
+      <Input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        placeholder="email"
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <p>{formik.errors.password}</p>
+      ) : null}
+      <Input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        placeholder="password"
+      />
+      <Button type="submit">Login</Button>
+    </Form>
+  );
+};
+
+export default Login;
