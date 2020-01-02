acbd6da58509dec9d72976ae48f3687d34345634
Noble Obioma <nobioma1@gmail.com>
2020-01-02 19:23:56 +0100

    Create Header and Global Styles

---
+++ src/styles/Header.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  position: fixed;
+  z-index: 999;
+  height: 80px;
+  padding: 15px;
+
+  a {
+    h1 {
+      font-size: 4rem;
+      font-family: 'Luckiest Guy', cursive;
+      color: #579afe;
+    }
+  }
+`;
