4afd593b6c4c060ff58a2ad12f3ff664ebbc220f
Nabeelah <noblenab13@gmail.com>
2020-01-02 21:27:54 +0100

    Set up actions

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 import { apiURL } from '../config';
 
-export const FETCH_LOCATIONS_LOADING = 'FETCH_LOCATIONS_LOADING';
-export const FETCH_LOCATIONS_SUCCESS = 'FETCH_LOCATIONS_SUCCESS';
-export const FETCH_LOCATIONS_FAILURE = 'FETCH_LOCATIONS_FAILURE';
+export const FETCH_EVENTS_LOADING = 'FETCH_EVENTS_LOADING';
+export const FETCH_EVENTS_SUCCESS = 'FETCH_EVENTS_SUCCESS';
+export const FETCH_EVENTS_FAILURE = 'FETCH_EVENTS_FAILURE';
 
 export const getEvents = events => ({
-  type: FETCH_LOCATIONS_SUCCESS,
+  type: FETCH_EVENTS_SUCCESS,
   payload: events.data
 });
 
 export const getAllEvents = () => async dispatch => {
-  dispatch({ type: FETCH_LOCATIONS_LOADING });
+  dispatch({ type: FETCH_EVENTS_LOADING });
   try {
     const eventsInfo = await axios.get(`${apiURL}/api/events`);
     dispatch(getEvents(eventsInfo));
   } catch (error) {
     dispatch({
-      type: FETCH_LOCATIONS_FAILURE,
+      type: FETCH_EVENTS_FAILURE,
       payload: err.response.data.message
     });
   }
