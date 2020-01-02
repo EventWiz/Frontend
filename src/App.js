34b665120ed56a1d0aafdf6835a678675712df3f
Noble Obioma <nobioma1@gmail.com>
2020-01-02 19:24:32 +0100

    Add Routes to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 const App = () => {
-  return <div className="App">Event Wiz</div>;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route exact path="/" render={() => 'this is the Landing'} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
