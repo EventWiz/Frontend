d81d8cc06283e18fa695033094ae7834564c7dd9
Nabeelah <noblenab13@gmail.com>
2020-01-02 21:26:07 +0100

    Fleshed out event actions

--- src/actions/events.js
+++ src/actions/events.js
@@ -4,3 +4,21 @@ import { apiURL } from '../config';
 export const FETCH_LOCATIONS_LOADING = 'FETCH_LOCATIONS_LOADING';
 export const FETCH_LOCATIONS_SUCCESS = 'FETCH_LOCATIONS_SUCCESS';
 export const FETCH_LOCATIONS_FAILURE = 'FETCH_LOCATIONS_FAILURE';
+
+export const getEvents = events => ({
+  type: FETCH_LOCATIONS_SUCCESS,
+  payload: events.data
+});
+
+export const getAllEvents = () => async dispatch => {
+  dispatch({ type: FETCH_LOCATIONS_LOADING });
+  try {
+    const eventsInfo = await axios.get(`${apiURL}/api/events`);
+    dispatch(getEvents(eventsInfo));
+  } catch (error) {
+    dispatch({
+      type: FETCH_LOCATIONS_FAILURE,
+      payload: err.response.data.message
+    });
+  }
+};
