82a374675603554cc69bb3c08d0cbfd8d42646bf
Noble Obioma <nobioma1@gmail.com>
2020-01-02 20:57:52 +0100

    Add Signup route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
 import { GlobalStyle } from './styles/GlobalStyle';
+import Signup from './components/Signup';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" render={() => 'this is the Landing'} />
         <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
       </Switch>
     </Router>
   );
