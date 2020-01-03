f0d8485585d7990192398969a0e187d5e00684d4
Noble Obioma <nobioma1@gmail.com>
2020-01-03 07:44:43 +0100

    Implement Private Route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { getToken } from '../utils/localStorage';
+
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => {
+      const currentUser = getToken();
+      if (!currentUser) {
+        return (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: {
+                prevLocation: props.history.location.pathname,
+                error: 'You need to login first!',
+              },
+            }}
+          />
+        );
+      }
+
+      return <Component {...props} />;
+    }}
+  />
+);
