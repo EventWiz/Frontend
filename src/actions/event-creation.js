a660b57c1476b1d51c5077191ed7e787dcaafef8
Nabeelah <noblenab13@gmail.com>
2020-01-03 02:24:18 +0100

    Create action for create event

---
+++ src/actions/event-creation.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { apiURL } from '../config';
+
+export const CREATE_EVENT_LOADING = 'CREATE_EVENT_LOADING';
+export const CREATE_EVENT_SUCCESS = 'CREATE_EVENT_SUCCESS';
+export const CREATE_EVENT_FAILURE = 'CREATE_EVENT_FAILURE';
+
+export const eventCreate = event => ({
+  type: CREATE_EVENT_SUCCESS,
+  payload: event
+});
+
+export const createEvent = details => async dispatch => {
+  try {
+    dispatch({ type: CREATE_EVENT_LOADING });
+    const eventDetails = await axios.post(`${apiURL}/api/events`, details);
+    dispatch(eventCreate(eventDetails));
+    return eventDetails;
+  } catch (error) {
+    dispatch({
+      type: CREATE_EVENT_FAILURE,
+      payload: error.response.data.message
+    });
+  }
+};
